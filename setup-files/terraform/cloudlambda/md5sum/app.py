59445cc9b8329f9b976bb1b1c0ba2c7f9183d544
k b <kbb29@yahoo.com>
2020-04-24 10:22:36 -0400

    ic

--- setup-files/terraform/cloudlambda/md5sum/app.py
+++ setup-files/terraform/cloudlambda/md5sum/app.py
@@ -6,9 +6,11 @@ app = Chalice(app_name='md5sum')
 @app.route('/{string}')
 def index(string):
     from subprocess import Popen, PIPE, STDOUT
-    import urllib2
-    string = urllib2.unquote(string).decode('utf8')
+    import urllib.request, urllib.error, urllib.parse
+    string = urllib.parse.unquote(string) #.decode('utf8')
     cmd = 'echo -n ' + string + ' | md5sum'
     p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT)
-    output = p.stdout.read()
+    output = p.stdout.read().decode('utf-8')
     return {"msg":cmd,"msg2":output}
+
+
